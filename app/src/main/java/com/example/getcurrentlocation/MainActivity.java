7e9f2dd86eec67af6fa01cc5486a5ef7e0719722
C17300916 <c17300916@mytudublin.ie>
2020-11-14 15:12:10 +0000

    Fixed rotation marker code

--- app/src/main/java/com/example/getcurrentlocation/MainActivity.java
+++ app/src/main/java/com/example/getcurrentlocation/MainActivity.java
@@ -8,8 +8,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.graphics.Matrix;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -20,10 +19,8 @@ import android.os.Handler;
 import android.os.SystemClock;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -35,12 +32,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     //initialize variable
     SupportMapFragment supportMapFragment;
     FusedLocationProviderClient client;
     boolean isMarkerRotating = false;
+
+    double azimuth = 0;
+    GoogleMap map;
+
     SensorManager sensorManager;
     private Sensor sensorAccelerometer;
     private Sensor sensorMagneticField;
@@ -56,6 +58,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        client = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation(0);
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
 
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -67,33 +76,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         matrixR = new float[9];
         matrixI = new float[9];
         matrixValues = new float[3];
+    }
 
+    @Override
+    protected void onResume() {
+        sensorManager.registerListener(this, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
 
-
-        //Assign variable
-        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.google_map);
-
-        //init fuse location
-        client = LocationServices.getFusedLocationProviderClient(this);
-
-        //check permission
-        if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            //when permission granted
-            //call method
-            getCurrentLocation(0);
-        } else {
-            //when permission denied
-            //request permission
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-        }
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(this, sensorAccelerometer);
+        sensorManager.unregisterListener(this, sensorMagneticField);
+        super.onPause();
     }
 
     public Bitmap resizeMapIcons(String iconName,int width, int height){
         Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(),getResources().getIdentifier(iconName, "drawable", getPackageName()));
-        Bitmap resizedBitmap = Bitmap.createScaledBitmap(imageBitmap, width, height, false);
-        return resizedBitmap;
+        imageBitmap = Bitmap.createScaledBitmap(imageBitmap, width, height, false);
+        Matrix matrix = new Matrix();
+        matrix.postRotate((float)(azimuth));
+        return Bitmap.createBitmap(imageBitmap, 0, 0, imageBitmap.getWidth(), imageBitmap.getHeight(), matrix, true);
     }
 
     private void getCurrentLocation(float azimuth) {
@@ -119,17 +123,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                         @Override
                         public void onMapReady(GoogleMap googleMap) {
                             //Initialize lat lng
+                            map = googleMap;
                             googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
                             //Bitmap marker_icon = BitmapFactory.decodeResource(getResources(),R.drawable.gps_arrow);
 
 
                             LatLng latLng = new LatLng(location.getLatitude()
-                                ,location.getLongitude());
+                                    ,location.getLongitude());
                             //create marker options
                             //Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 
-                            Marker marker = googleMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons("arrow",100,100))));
+
                             //changed icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
 
                             //MarkerOptions options = new MarkerOptions().position(latLng)
@@ -142,11 +147,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                             LatLng newLocaation;
                             LatLng oldLocation;
 
-
-                            rotateMarker(marker, azimuth);
-
                             //add marker on map
-                           // googleMap.addMarker(options);
+                            // googleMap.addMarker(options);
                         }
                     }));
 
@@ -156,61 +158,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    private double bearingBetweenLocations(LatLng latLng1,LatLng latLng2) {
 
-        double PI = 3.14159;
-        double lat1 = latLng1.latitude * PI / 180;
-        double long1 = latLng1.longitude * PI / 180;
-        double lat2 = latLng2.latitude * PI / 180;
-        double long2 = latLng2.longitude * PI / 180;
-
-        double dLon = (long2 - long1);
-
-        double y = Math.sin(dLon) * Math.cos(lat2);
-        double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1)
-                * Math.cos(lat2) * Math.cos(dLon);
-
-        double brng = Math.atan2(y, x);
-
-        brng = Math.toDegrees(brng);
-        brng = (brng + 360) % 360;
-
-        return brng;
-    }
-
-    private void rotateMarker(final Marker marker, final float toRotation) {
-        if(!isMarkerRotating) {
-            final Handler handler = new Handler();
-            final long start = SystemClock.uptimeMillis();
-            final float startRotation = marker.getRotation();
-            final long duration = 2000;
-
-            final Interpolator interpolator = new LinearInterpolator();
-
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    isMarkerRotating = true;
-
-                    long elapsed = SystemClock.uptimeMillis() - start;
-                    float t = interpolator.getInterpolation((float) elapsed / duration);
-
-                    float rot = t * toRotation + (1 - t) * startRotation;
-
-                    float bearing =  -rot > 180 ? rot / 2 : rot;
-
-                    marker.setRotation(bearing);
-
-                    if (t < 1.0) {
-                        // Post again 16ms later.
-                        handler.postDelayed(this, 16);
-                    } else {
-                        isMarkerRotating = false;
-                    }
-                }
-            });
-        }
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -224,36 +172,46 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event)
-    {
-        // It is good practice to check that we received the proper sensor event
-        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR)
-        {
-            // Convert the rotation-vector to a 4x4 matrix.
-            SensorManager.getRotationMatrixFromVector(matrixR, event.values);
-            SensorManager.remapCoordinateSystem(matrixR,
-                    SensorManager.AXIS_X, SensorManager.AXIS_Z,
-                    matrixR);
+    public void onSensorChanged(SensorEvent event) {
+        // TODO Auto-generated method stub
+
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                for (int i = 0; i < 3; i++) {
+                    valuesAccelerometer[i] = event.values[i];
+                }
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                for (int i = 0; i < 3; i++) {
+                    valuesMagneticField[i] = event.values[i];
+                }
+                break;
+        }
+
+        boolean success = SensorManager.getRotationMatrix(
+                matrixR,
+                matrixI,
+                valuesAccelerometer,
+                valuesMagneticField);
+
+        if (success) {
+            double tempAz = matrixValues[0];
             SensorManager.getOrientation(matrixR, matrixValues);
-
-            // Optionally convert the result from radians to degrees
-            matrixValues[0] = (float) Math.toDegrees(matrixValues[0]);
-            matrixValues[1] = (float) Math.toDegrees(matrixValues[1]);
-            matrixValues[2] = (float) Math.toDegrees(matrixValues[2]);
-
-            /*tv.setText(" Yaw: " + matrixValues[0] + "\n Pitch: "
-                        + matrixValues[1] + "\n Roll (not used): "
-                    + matrixValues[2]);*/
-
-            getCurrentLocation(matrixValues[0]);
-
-
-
+            azimuth = Math.toDegrees(matrixValues[0]);
+            double pitch = Math.toDegrees(matrixValues[1]);
+            double roll = Math.toDegrees(matrixValues[2]);
+            if(!( Math.toDegrees(tempAz) == azimuth) ){
+                if(! (map == null)) {
+                    map.clear();
+                    LatLng latLng = map.getCameraPosition().target;
+                    Marker marker = map.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons("arrow", 100, 100))));
+                }
+            }
         }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
